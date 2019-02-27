f992a378221389a5f2c27e4b03fcaf98ec41b041
wulinyun <lin.wu@landasoft.com>
2019-02-27 17:47:04 +0800

    提交redis的其它单元测试

--- src/test/java/com/landasoft/mas/demo/redis/SpringBootDemoRedisApplicationTests.java
+++ src/test/java/com/landasoft/mas/demo/redis/SpringBootDemoRedisApplicationTests.java
@@ -1,7 +1,11 @@
 package com.landasoft.mas.demo.redis;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -41,19 +45,36 @@ public class SpringBootDemoRedisApplicationTests {
 		map.put("map1", "map1");
 		map.put("map2", "map2");
 		map.put("map3", redisUserModel);
-		System.out.println(redisService.hmset("testRedisMap", map, 120));
+		System.out.println(redisService.hmset("testRedisMap", map, 60));
 		System.out.println(redisService.hmget("testRedisMap"));
 		System.out.println(((RedisUserModel)redisService.hget("testRedisMap", "map3")).getUserName());
 	}
 	@Test
 	public void testRedisSet() {
-		System.out.println(redisService.set("setString", "setString", 60));
-		System.out.println(redisService.get("setString"));
+		Set<Object> set = new HashSet<>();
+		set.add("set1");
+		set.add("set2");
+		set.add("set3");
+		RedisUserModel  redisUserModel = new RedisUserModel();
+		redisUserModel.setUserName("wulinyun");
+		redisUserModel.setUserPaasword("5888888");
+		set.add(redisUserModel);
+		System.out.println(redisService.sSet("testRedisSet", 60, set.toArray()));
+		redisService.setRemove("testRedisSet", redisUserModel);
+		System.out.println(redisService.sGet("testRedisSet"));
 	}
 	@Test
 	public void testRedisList() {
-		System.out.println(redisService.set("setString", "setString", 60));
-		System.out.println(redisService.get("setString"));
+		List<Object> list = new ArrayList<>();
+		list.add("11111");
+		list.add("22222");
+		list.add("33333");
+		RedisUserModel  redisUserModel = new RedisUserModel();
+		redisUserModel.setUserName("55555");
+		redisUserModel.setUserPaasword("6666");
+		//list.add(redisUserModel);
+		System.out.println(redisService.lSet("testRedisList", list, 60));
+		System.out.println(redisService.lGet("testRedisList", 0, -1));
 	}
 }
 
