d6fc7ea637791b049f7ecf787b838948ee37b8c9
wulinyun <lin.wu@landasoft.com>
2019-02-27 14:44:24 +0800

    springboot整合redis代码

--- src/main/java/com/landasoft/mas/demo/redis/SpringBootDemoRedisApplication.java
+++ src/main/java/com/landasoft/mas/demo/redis/SpringBootDemoRedisApplication.java
@@ -2,8 +2,6 @@ package com.landasoft.mas.demo.redis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-@EnableEurekaClient
 @SpringBootApplication
 public class SpringBootDemoRedisApplication {
 
